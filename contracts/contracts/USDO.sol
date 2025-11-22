280726b17f5109b59f87823122841586dd89379a
0xKairn <louis.prigent77.fr@gmail.com>
2025-11-22 02:45:29 +0000

    ADD: custom lzReceive on OFT

--- contracts/contracts/USDO.sol
+++ contracts/contracts/USDO.sol
@@ -3,22 +3,29 @@ pragma solidity ^0.8.22;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
-import { EIP3009Extended } from "./EIP3009Extended.sol";
-import { EIP712 } from "./lib/EIP712.sol";
+import { OApp, Origin } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { OFTMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTMsgCodec.sol";
+import { EIP3009Extended } from "./EIP3009Extended.sol";
+import { EIP712 } from "./lib/EIP712.sol";
 import { IERC20Internal } from "./lib/IERC20Internal.sol";
 
 contract USDO is OFT, EIP3009Extended {
     using SafeERC20 for ERC20;
+    using OFTMsgCodec for bytes;
+    using OFTMsgCodec for bytes32;
 
     ERC20 public USDC;
     uint256 public _decimals = 6;
 
+    error InsufficientBalance();
     error InsufficientAllowance();
+    error InvalidReceiver();
 
     event Deposit(address indexed account, uint256 amount);
     event Withdraw(address indexed account, uint256 amount);
+    event x402Bridge(address indexed from, address indexed to, bytes32 indexed nonce, uint256 amount);
 
     constructor(
         string memory _name,
@@ -36,11 +43,41 @@ contract USDO is OFT, EIP3009Extended {
     }
 
     // =====================================================
-    // ----------- EIP3009Extended Implementations ----------
+    // -------------- OFTCore Implementations --------------
     // =====================================================
 
-    function setAuthorizedRouter(address router, bool authorized) external override onlyOwner {
-        authorizedRouters[router] = authorized;
+    function _lzReceive(
+        Origin calldata _origin,
+        bytes32 _guid,
+        bytes calldata _message,
+        address _executor, // @dev unused in the default implementation.
+        bytes calldata _extraData // @dev unused in the default implementation.
+    ) internal virtual override {
+        bytes memory composeMsg = OFTMsgCodec.composeMsg(_message);
+        (, bytes32 nonce, address receiver, address sender) = abi.decode(
+            composeMsg,
+            (address, bytes32, address, address)
+        );
+
+        require(!_authorizationStates[sender][nonce], _AUTHORIZATION_USED_ERROR);
+
+        _authorizationStates[sender][nonce] = true;
+        emit AuthorizationUsed(sender, nonce);
+
+        address toAddress = OFTMsgCodec.sendTo(_message).bytes32ToAddress();
+        uint256 amountLD = _toLD(OFTMsgCodec.amountSD(_message));
+
+        if (toAddress != address(this)) {
+            revert InvalidReceiver();
+        }
+
+        if (USDC.balanceOf(address(this)) < amountLD) {
+            revert InsufficientBalance();
+        }
+
+        USDC.safeTransfer(receiver, amountLD);
+
+        emit x402Bridge(sender, receiver, nonce, amountLD);
     }
 
     // =====================================================
@@ -64,6 +101,14 @@ contract USDO is OFT, EIP3009Extended {
     // for test purposes only, should be remove in prod
     function retrieveERC20(address tokenAddress, uint256 amount) external onlyOwner {
         ERC20(tokenAddress).safeTransfer(owner(), amount);
+    }
+
+    // =====================================================
+    // ----------- EIP3009Extended Implementations ----------
+    // =====================================================
+
+    function setAuthorizedRouter(address router, bool authorized) external override onlyOwner {
+        authorizedRouters[router] = authorized;
     }
 
     // =====================================================
